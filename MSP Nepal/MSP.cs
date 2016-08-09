31900457bcab95a842f5711ba9b28411044da0b3
harimanok <codescloudofficial@gmail.com>
2016-08-09 11:25:20 +0545

    class to store MSP info

---
+++ MSP Nepal/MSP.cs
@@ -0,0 +1,14 @@
+﻿namespace MSP_Nepal
+{
+    internal class MSP
+    {
+
+        public string FullName, College, Bio;
+        public MSP(string name, string clg, string bio) 
+        {
+            FullName = name;
+            College = clg;
+            bio = Bio;
+        }
+    }
+}
