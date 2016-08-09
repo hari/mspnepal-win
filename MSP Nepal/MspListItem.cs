44ca71330546c34121ca19404ada7d19cf4c589d
harimanok <codescloudofficial@gmail.com>
2016-08-09 21:46:12 +0545

    Listview item for MSP

---
+++ MSP Nepal/MspListItem.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MSP_Nepal
+{
+    /// <summary>
+    /// This class will represent the listviewitem style of the MSP item in the listview
+    /// </summary>
+    internal class MspListItem : StackPanel
+    {
+        private TextBlock TxtName = new TextBlock(), TxtCollege = new TextBlock();
+        public MspListItem(string name, string college)
+        {
+            Padding = new Windows.UI.Xaml.Thickness(0, 4, 0, 4);
+            TxtName.Text = name;
+            TxtName.FontSize = 20;
+            TxtName.Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(240, 40, 40, 40));
+            TxtCollege.Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(190, 40, 40, 40));
+            TxtCollege.Text = college;
+            TxtCollege.FontSize = 13;
+            Children.Add(TxtName);
+            Children.Add(TxtCollege);
+        }
+    }
+}
