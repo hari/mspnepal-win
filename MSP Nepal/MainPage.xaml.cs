57f56d3a11bc15eac5d8871b05afcfb52633839a
harimanok <codescloudofficial@gmail.com>
2016-08-09 21:39:12 +0545

    implement search

--- MSP Nepal/MainPage.xaml.cs
+++ MSP Nepal/MainPage.xaml.cs
@@ -10,17 +10,58 @@ namespace MSP_Nepal
         public MainPage()
         {
             InitializeComponent();
+
             if (MspList.IsFirstRun())
             {
-                ShowLoading();
                 LoadAndSave();
                 MspList.SetFirstRun(false);
             }
-
+            for (char a = 'A'; a <= 'Z'; a++)
+            {
+                var b = new TextBlock();
+                b.Text = a.ToString();
+                b.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
+                b.Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(255, 100, 100, 200));
+                b.PointerPressed += (s, e) =>
+                {
+                    Show((s as TextBlock).Text, true);
+                };
+                A2Z.Children.Add(b);
+            }
+            Show();
         }
-        private void ShowLoading()
-        {
 
+        private void Show(string Str = null, bool Filter = false)
+        {
+            ListMsp.Items.Clear();
+            if (Str != null)
+            {
+                Str = Str.ToLower();
+            }
+            MspList.GetMSP().ForEach(msp =>
+            {
+                MspListItem mlt = null;
+                if (Str == null)
+                {
+                    mlt = new MspListItem(msp.FullName, msp.College);
+                }
+                else
+                {
+                    if (Filter && (msp.FullName.ToLower().Substring(0,1).Equals(Str) || msp.College.ToLower().Substring(0, 1).Equals(Str)))
+                    {
+                        mlt = new MspListItem(msp.FullName, msp.College);
+                    }
+                    if (!Filter && (msp.FullName.ToLower().Contains(Str) || msp.College.ToLower().Contains(Str)))
+                    {
+                        mlt = new MspListItem(msp.FullName, msp.College);
+                    }
+                }
+                if (mlt != null) ListMsp.Items.Add(mlt);
+            });
+            if (ListMsp.Items.Count == 0)
+            {
+                ListMsp.Items.Add(new MspListItem("Ooops!", "No result found."));
+            }
         }
 
         /// <summary>
@@ -30,9 +71,9 @@ namespace MSP_Nepal
         {
             List<MSP> list = new List<MSP>();
             JsonArray jsonList = JsonArray.Parse(await MspList.GetData());
-            for(int i = jsonList.Count - 1;i >= 0; --i)
+            for (int i = jsonList.Count - 1; i >= 0; --i)
             {
-                
+
                 JsonObject obj = jsonList.GetObjectAt((uint)i);
                 list.Add(new MSP(
                      obj.GetNamedString("full_name"),
@@ -42,6 +83,28 @@ namespace MSP_Nepal
             }
             MspList.AddAll(list);
         }
-        
+
+        private async void BtnSearch_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (TxtSearch.Text.Trim().Length == 0)
+            {
+                Windows.UI.Popups.MessageDialog msg = new Windows.UI.Popups.MessageDialog("Nothing to search");
+                await msg.ShowAsync();
+            }
+            Show(TxtSearch.Text);
+        }
+
+        private void TxtSearch_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            if (TxtSearch.Text.Length == 0) Show();
+        }
+
+        private void TxtSearch_KeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key.ToString().Equals("Enter") && TxtSearch.Text.Trim().Length > 0)
+            {
+                Show(TxtSearch.Text);
+            }
+        }
     }
 }
